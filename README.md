00643c9b0f83dc442896cf7886ccef31862e98e6
Jacek Gębal <jgebal@gmail.com>
2016-02-03 23:37:36 +0000

    Updated Readme MD. Updated profiler code and outcomes.

--- README.md
+++ README.md
@@ -39,9 +39,11 @@ create or replace type a_demo_join_collection as table of a_demo_join_object;
 # Description
 
 There are currently three different join methods implemented:
- - Nested loops outer join with full scan of the inner collection. The method has n*m complexity, causing computation intensity to grow exponentially.
- - Nested loops outer join with index-like scan of the inner collection. First an index is build on inner collection, then access is done via index-path. Complexity ~= x*m + y*(m+n) where x is number of steps needed to build index and y is the number of steps needed to link the two.
- - Sort-merge outer join. First both collections are sorted, then each element is accessed exactly once while joining. Complexity ~= a*(m+n) + b*(m+n) where a is number of steps needed to sort the collection and b is the number of steps needed to link the two.  
+ - Nested loops outer join with full scan of the inner collection. The method has complexity `n*m`, causing computation intensity to grow exponentially.
+ - Nested loops outer join with index-like scan of the inner collection. First an index is build on inner collection, then access is done via index-path. Complexity ` x*m + y*(m+n)` where x is number of steps needed to build index and y is the number of steps needed to link the two.
+ - Sort-merge outer join. First both collections are sorted, then each element is accessed exactly once while joining. Complexity ` a*(m+n) + b*(m+n)` where a is number of steps needed to sort the collection and b is the number of steps needed to link the two.
+
+Both sort-merge and nested loops with index should provide quite linear performance with the growth of amount of data processed.
 
 # Usage
 
@@ -56,7 +58,8 @@ select *
             a_demo_object(1,'a')
         )
      )
-  )
+  );
+
 --join 100 sample rows with 1000 sample rows
 select * from table( collection_outer_join.nl_index_scan_join(100, 1000) );
 select * from table( collection_outer_join.sort_merge_join(100, 1000) );
@@ -69,9 +72,19 @@ Additional package collection_gen was added only for demo and testing purposes.
 
 # Performance
 
+RUN_COMMENT|TOTAL_TIME_MILI_SEC
+-----------|-------------------
+nl_full_scan_join 1,000 parents 20,000 children|178,320
+nl_index_scan_join 1,000 parents 20,000 children|900
+sort_merge_join 1,000 parents 20,000 children|1,130
+nl_index_scan_join 40,000 parents 400,000 children|20,720
+sort_merge_join 40,000 parents 400,000 children|27,330
+nl_index_scan_join 10,000 parents 1,000,000 children|46,810
+sort_merge_join 10,000 parents 1,000,000 children|70,040
+
 The code was profiled using DBMSL_PROFILER.
 - The `nl_full_scan_join` is slowest
 - The `nl_index_scan_join` is fastest 
 - The `sort_merge_join` would be as fast as nested loops, but requires sorting that takes extra hit on performance.
 
-
+[Here][sql/profiler_outcomes.html] you may see the full outcomes of the profiler runs. 
--- sql/profile_code.sql
+++ sql/profile_code.sql
@@ -10,43 +10,44 @@ alter package collection_outer_join compile debug;
 declare
    v_dummy integer;
 begin
---    dbms_profiler.START_PROFILER('nl_full_scan_join 2,000 parents 20,000 children');
---    select count(1) into v_dummy
---    from table( collection_outer_join.nl_full_scan_join(2000, 20000) );
---    dbms_profiler.STOP_PROFILER();
-
-   dbms_profiler.START_PROFILER('nl_index_scan_join 2,000 parents 20,000 children');
+   dbms_profiler.START_PROFILER('nl_full_scan_join 1,000 parents 20,000 children');
    select count(1) into v_dummy
-   from table( collection_outer_join.nl_index_scan_join(2000, 20000) );
+   from table( collection_outer_join.nl_full_scan_join(1000, 20000) );
    dbms_profiler.STOP_PROFILER();
 
-   dbms_profiler.START_PROFILER('sort_merge_join 2,000 parents 20,000 children');
+   dbms_profiler.START_PROFILER('nl_index_scan_join 1,000 parents 20,000 children');
    select count(1) into v_dummy
-   from table( collection_outer_join.sort_merge_join(2000, 20000) );
+   from table( collection_outer_join.nl_index_scan_join(1000, 20000) );
    dbms_profiler.STOP_PROFILER();
 
---    dbms_profiler.START_PROFILER('nl_index_scan_join 100,000 parents 400,000 children');
---    select count(1) into v_dummy
---    from table( collection_outer_join.nl_index_scan_join(100000, 400000) );
---    dbms_profiler.STOP_PROFILER();
---
---    dbms_profiler.START_PROFILER('sort_merge_join 100,000 parents 400,000 children');
---    select count(1) into v_dummy
---    from table( collection_outer_join.sort_merge_join(100000, 400000) );
---    dbms_profiler.STOP_PROFILER();
---
---    dbms_profiler.START_PROFILER('nl_index_scan_join 10,000 parents 500,000 children');
---    select count(1) into v_dummy
---    from table( collection_outer_join.nl_index_scan_join(10000, 500000) );
---    dbms_profiler.STOP_PROFILER();
---
---    dbms_profiler.START_PROFILER('sort_merge_join 10,000 parents 500,000 children');
---    select count(1) into v_dummy
---    from table( collection_outer_join.sort_merge_join(10000, 500000) );
---    dbms_profiler.STOP_PROFILER();
+   dbms_profiler.START_PROFILER('sort_merge_join 1,000 parents 20,000 children');
+   select count(1) into v_dummy
+   from table( collection_outer_join.sort_merge_join(1000, 20000) );
+   dbms_profiler.STOP_PROFILER();
+
+   dbms_profiler.START_PROFILER('nl_index_scan_join 40,000 parents 400,000 children');
+   select count(1) into v_dummy
+   from table( collection_outer_join.nl_index_scan_join(40000, 400000) );
+   dbms_profiler.STOP_PROFILER();
+
+   dbms_profiler.START_PROFILER('sort_merge_join 40,000 parents 400,000 children');
+   select count(1) into v_dummy
+   from table( collection_outer_join.sort_merge_join(40000, 400000) );
+   dbms_profiler.STOP_PROFILER();
+
+   dbms_profiler.START_PROFILER('nl_index_scan_join 10,000 parents 1,000,000 children');
+   select count(1) into v_dummy
+   from table( collection_outer_join.nl_index_scan_join(10000, 1000000) );
+   dbms_profiler.STOP_PROFILER();
+
+   dbms_profiler.START_PROFILER('sort_merge_join 10,000 parents 1,000,000 children');
+   select count(1) into v_dummy
+   from table( collection_outer_join.sort_merge_join(10000, 1000000) );
+   dbms_profiler.STOP_PROFILER();
 end;
 /
 
+select run_comment, run_total_time/1000000 as total_time_mili_sec from plsql_profiler_runs order by runid;
 
 select
        plsql_profiler_runs.run_comment,
